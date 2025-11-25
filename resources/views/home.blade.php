7010074db937b566d726a13eaeba87dca9e17f2b
NurAzisahBasir <nurazisahicha02@gmail.com>
2025-11-25 21:48:58 +0800

    hapus layout.blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -41,7 +41,7 @@
         <header class="fixed top-0 left-0 md:left-64 right-0 h-16 bg-white shadow flex items-center justify-between px-6 z-50">
             <div class="font-medium text-sm">Dashboard</div>
             <button class="px-4 py-1.5 bg-orange-500 text-white text-xs font-semibold rounded-full hover:bg-orange-600"
-             onclick="window.location.href='{{route('login')}}'">
+             onclick="window.location.href={{route('login')}}">
                 LOGIN
             </button>
         </header>
